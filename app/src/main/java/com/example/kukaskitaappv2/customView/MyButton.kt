bba44edfe4b9bd4d1141f76658c4e71a908a53ae
Alfianrma <alfianrma77@gmail.com>
2023-06-01 13:41:10 +0700

    first commit

---
+++ app/src/main/java/com/example/kukaskitaappv2/customView/MyButton.kt
@@ -0,0 +1,37 @@
+package com.example.kukaskitaappv2.customView
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.example.kukaskitaappv2.R
+
+class MyButton: AppCompatButton {
+    private lateinit var btnBackground : Drawable
+    private var txtColor: Int = 0
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        background = btnBackground
+        setTextColor(txtColor)
+        gravity = Gravity.CENTER
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, R.color.dark_green)
+        btnBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+    }
+}
---
+++ app/src/main/java/com/example/kukaskitaappv2/customView/MyEditText.kt
@@ -0,0 +1,59 @@
+package com.example.kukaskitaappv2.customView
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.example.kukaskitaappv2.R
+
+class MyEditText : AppCompatEditText {
+    private lateinit var editTextBackground: Drawable
+    private lateinit var editTextErrorBackground: Drawable
+    private var isError = false
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context, attrs, defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        background = if (isError) editTextErrorBackground else editTextBackground
+    }
+
+    private fun init() {
+        editTextBackground = ContextCompat.getDrawable(context, R.drawable.bg_edit_text) as Drawable
+        editTextErrorBackground = ContextCompat.getDrawable(context, R.drawable.bg_et_error) as Drawable
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if (text.isNullOrEmpty()) {
+                    error = resources.getString(R.string.field_cannot_empty)
+                    isError = true
+                }
+            }
+        })
+    }
+
+}
